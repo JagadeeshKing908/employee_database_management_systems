5a1a413d8d4baa11c70572c13d77d56e1e1fc5ef
JagadeeshKing908 <jagadeeshcloud0@gmail.com>
2026-07-15 13:08:59 +0530

    Create init.sql

---
+++ Docker/app/Db/init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    department VARCHAR(100)
+);
+
+INSERT INTO employee(name,email,department)
+VALUES
+('Jagadeesh','jagadeesh@gmail.com','DevOps');
