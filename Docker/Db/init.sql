6245f4ca68b94f157e7d81cd21053b9cea7b4562
JagadeeshKing908 <jagadeeshcloud0@gmail.com>
2026-07-15 18:32:18 +0530

    Update init.sql

--- Docker/Db/init.sql
+++ Docker/Db/init.sql
@@ -1,10 +1,10 @@
-CREATE TABLE employee (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100),
-    password VARCHAR(100),
-    department VARCHAR(100)
-);
+-- CREATE TABLE employee (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(100),
+--     email VARCHAR(100),
+--     password VARCHAR(100),
+--     department VARCHAR(100)
+-- );
 
 INSERT INTO employee (email, password, role)
 VALUES ('admin@gmail.com', '123', 'ADMIN');
